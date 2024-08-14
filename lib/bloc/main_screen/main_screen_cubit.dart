c4a7b5a14324909d5fe7596433d738db4ea6c5e8
duccv <duc.cv@zinza.com.vn>
2024-08-14 09:54:22 +0700

    feat: resize the image if it reaches the size limit

--- lib/bloc/main_screen/main_screen_cubit.dart
+++ lib/bloc/main_screen/main_screen_cubit.dart
@@ -2,6 +2,10 @@ import 'dart:io';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../../domain/repositories/upload_repository.dart';
 import '../../models/user_profile.dart';
@@ -15,10 +19,12 @@ class MainScreenCubit extends Cubit<MainScreenState> {
   Future<bool> postImageToLocket(UserProfile userProfile, File image, String caption) async {
     try {
       emit(const MainScreenUploading());
-      await _uploadRepository.postImageToLocket(userProfile, image, caption);
+      debugPrint("Before: ${image.lengthSync()}");
+      await _uploadRepository.postImageToLocket(userProfile, await _resizeImage(image), caption);
       emit(const MainScreenUploadedSuccess());
       return true;
     } catch (e) {
+      print("error: $e");
       emit(MainScreenUploadedFailure(error: e.toString()));
       return false;
     }
@@ -34,5 +40,29 @@ class MainScreenCubit extends Cubit<MainScreenState> {
       emit(MainScreenUploadedFailure(error: e.toString()));
       return false;
     }
+  }
+
+  Future<File> _resizeImage(File file) async {
+    final imageSize = file.lengthSync();
+    const bitPerMB = 1000000;
+    // Resize image to 1MB
+    if (imageSize < bitPerMB) {
+      return file;
+    }
+
+    final newQuality = 100 - (bitPerMB / imageSize * 100).round();
+
+    var result = await FlutterImageCompress.compressWithFile(
+      file.absolute.path,
+      quality: newQuality,
+    );
+
+    debugPrint("Before: ${file.lengthSync()}");
+    debugPrint("After: ${result!.length}");
+
+    final tempDir = await getTemporaryDirectory();
+    File newFile = await File('${tempDir.path}/image.png').create();
+    newFile.writeAsBytesSync(result);
+    return newFile;
   }
 }
